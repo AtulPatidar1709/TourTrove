2f8441032e4d6bf530fe404e4e9ebd3f56ff934c
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-04-29 22:14:20 +0530

    Country and  images Section

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,12 +1,17 @@
 'use client'
-
-import useRentModel from "@/app/hooks/useRentModel"
-import Modal from "./Modal"
 import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+import Modal from "./Modal"
+import useRentModel from "@/app/hooks/useRentModel"
+import CountrySelect from "../Input/CountrySelect";
 import Heading from "../Heading";
+
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../Input/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
+import Counter from "../Input/Counter";
+import ImageUpload from "../Input/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -45,7 +50,20 @@ const RentModal = () => {
         },
     })
 
+
+
     const category = watch('category');
+    const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
+
+    const Map = useMemo(() =>
+        dynamic(() => import('../Map'), {
+            ssr: false
+        }), [location]
+    );
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldValidate: true,
@@ -107,11 +125,75 @@ const RentModal = () => {
         </div>
     )
 
+    if (step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+                <CountrySelect
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map
+                    center={location?.latlng}
+                />
+            </div>
+        )
+    }
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have?"
+                />
+                <Counter
+                    title="Guests"
+                    subtitle="How many guests do you allow?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                />
+                <hr />
+                <Counter
+                    title="Rooms"
+                    subtitle="How many rooms do you have?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                />
+                <hr />
+                <Counter
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place look like!"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                />
+            </div>
+        )
+    }
+
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLable={actionLabel}
             secondaryActionLable={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [`avatars.githubusercontent.com`, "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
