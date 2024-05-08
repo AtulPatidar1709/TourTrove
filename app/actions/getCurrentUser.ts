e36d74ba42fa5bd838e48b9145f455a68554a335
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-05-09 01:24:41 +0530

    NextAuth Error

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../libs/config/authOptions";
 
 import prisma from "@/app/libs/Prismadb";
 
