28927b154e4fce02ffe707eb48911843942d0a29
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-05-09 00:43:57 +0530

    Deployment Error's Fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -65,6 +65,6 @@ const authOptions: AuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
 };
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
