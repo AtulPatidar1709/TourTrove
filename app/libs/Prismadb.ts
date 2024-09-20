5d886febfc5c28db68aeab4eb51b57592511923c
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-09-21 01:09:46 +0530

    Final Changes for Deployment

--- app/libs/Prismadb.ts
+++ app/libs/Prismadb.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined
+// Add a check to ensure that the Prisma client is not recreated unnecessarily.
+let prisma: PrismaClient;
 
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    if (!(global as any).prisma) {
+        (global as any).prisma = new PrismaClient();
+    }
+    prisma = (global as any).prisma;
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
-
-export default client;
+export default prisma;
