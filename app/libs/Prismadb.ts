1f4da2748c2362e17cd123ce6dfb5cdc4dee5808
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-09-21 01:27:38 +0530

    Final Changes for Deployment

--- app/libs/Prismadb.ts
+++ app/libs/Prismadb.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 // Add a check to ensure that the Prisma client is not recreated unnecessarily.
 let prisma: PrismaClient;
@@ -6,6 +6,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
 } else {
+    // In development, use a global variable to prevent creating multiple instances of PrismaClient
     if (!(global as any).prisma) {
         (global as any).prisma = new PrismaClient();
     }
