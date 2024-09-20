6c549550ae3b43a4036650fde9d52322bfb43923
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-09-21 01:16:19 +0530

    Final Changes for Deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/Navbar/Navbar";
-import RegisterModal from "./components/Modals/RegisterModal";
-import LoginModel from "./components/Modals/LoginModel";
-import RentModal from "./components/Modals/RentModal";
-import ToasterProvider from "./Providers/ToasterProvider";
-import getCurrentUser from "./actions/getCurrentUser";
-import SearchModal from "./components/Modals/SearchModal";
 import { Suspense } from "react";
 import Loader from "./components/Loader";
+import LoginModel from "./components/Modals/LoginModel";
+import RegisterModal from "./components/Modals/RegisterModal";
+import RentModal from "./components/Modals/RentModal";
+import SearchModal from "./components/Modals/SearchModal";
+import Navbar from "./components/Navbar/Navbar";
+import "./globals.css";
+import ToasterProvider from "./Providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
